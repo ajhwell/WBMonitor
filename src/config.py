4d96cee34e87a0e5c55fe86ad502dbee2f721e2a
ajhwell <ajhwell@qq.com>
2022-03-16 08:41:16 +0800

    Update config.py
    k

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 import os
-PUSH_KEY = os.getenv('PUSH_KEY')
+PUSH_KEY = os.getenv('PDU7633Tc2osfzQnh8We5eY5sL3b1oTYUr5Z9oGM')
 WB_USERIDS = os.getenv('WB_USERIDS')
 WB_USERID = WB_USERIDS.split(",")
