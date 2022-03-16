5e76e0a72c08884a35f02c59279f04e3dce86680
ajhwell <ajhwell@qq.com>
2022-03-16 08:39:48 +0800

    Update startup.py
    11

--- src/startup.py
+++ src/startup.py
@@ -8,14 +8,17 @@ def timer(n,task):
         count+=1
         if(count>times):
             msg = 'Monitor End'
-            requests.get('https://sc.ftqq.com/' + PUSH_KEY + '.send?text=' + msg)
+            requests.get('https://api2.pushdeer.com/message/push?pushkey=' + PUSH_KEY + '&text=' + msg)            
+
             break
         result = task.MonitorWB()
         print(result[0])
         if(result[0]):
             title = result[1]
             desp = result[2]
-            requests.get('https://sc.ftqq.com/' + PUSH_KEY + '.send?text=' + title +'&desp='+desp)
+            requests.get('https://api2.pushdeer.com/message/push?pushkey=' + PUSH_KEY + '&text=' + title +'&desp='+desp+'&type=markdown')
+            
+
             print(f'succ get newWeibo in {count}')
         else:
             print(f'keep get and now {count}')
